9dba58fef0faf79223400a7585b32da62a2c6e91
MashR3 <michaelash17@googlemail.com>
2017-10-05 22:39:22 +0100

    Relationships work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+u1 = User.create!(:email => "mash@spartaglobal.com", :password => "sparta")
+
+
+t1 = Task.create!(item: "Clean shoes", user_id: u1.id)
+t2 = Task.create!(item: "Finish homework", user_id: u1.id)
